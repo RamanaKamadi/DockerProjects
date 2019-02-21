c2e35dbad4d9566457a5bc2aae86f8dac62d8d78
Akkamahadevi <Akkamahadevi.s@exilant.com>
2019-02-21 14:47:21 +0530

    docker whole projects

---
+++ DockerWorkspace/docker-spring-boot/src/main/java/com/demo/docker/dockerspringboot/controller/DemoDocker.java
@@ -0,0 +1,17 @@
+package com.demo.docker.dockerspringboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rest/docker/hello")
+public class DemoDocker {
+
+	@GetMapping
+	public String hello() {
+		
+		return "hello youtube";
+	}
+	
+}
